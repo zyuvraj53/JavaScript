1b8f55eab9d23bde6d5fc63424df3f1b3a7e1e48
zyuvraj53 <77329747+zyuvraj53@users.noreply.github.com>
2021-09-04 13:38:01 +0530

    initial commit

---
+++ Chart.js/Chart.jsTutTraversy/1-index.js
@@ -0,0 +1,37 @@
+let myChart = document.getElementById("myChart").getContext("2d");
+
+let massPopChart = new Chart(myChart, {
+  type: "polarArea", //?bar, HorizontalBar, pie, line, doughnut, radar, polarArea
+  data: {
+    labels: labelsForCities,
+    datasets: [
+      {
+        label: "Population",
+        data: labelsForPopulation,
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+        borderWidth: 1,
+        hoverBorderWidth: 3,
+        hoverBorderColor: borderColor,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      title: {
+        display: true,
+        text: "Largest Cities in Massachusets",
+        padding: {
+          top: 10,
+          bottom: 10,
+        },
+      },
+      legend: {
+        position: "right",
+      },
+      tooltips: {
+        enabled: true,
+      }
+    },
+  },
+});
