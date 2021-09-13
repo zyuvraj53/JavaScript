f444cc3b5497fb41194a4cb8f831254b5a07248e
Yuvraj Singh <zyuvraj53@gmail.com>
2021-09-13 09:18:33 +0530

    .

--- WorkingWithData/sketch.js
+++ WorkingWithData/sketch.js
@@ -1,7 +1,22 @@
+var weather;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(200, 200);
+  // loadJSON("http://api.open-notify.org/astros.json", gotData)
+  loadJSON("https://api.openweathermap.org/data/2.5/weather?q=London&appid=3e42d5b9cea24f04dbc62664c5b336fb&units=metric", gotWeatherData);
+}
+
+function gotData(data){
+  // console.log(data);
+}
+
+function gotWeatherData(data){
+  weather = data;
 }
 
 function draw() {
-  background(220);
+  background(125, 0, 255);
+  if(weather){
+    ellipse()
+  }
 }
